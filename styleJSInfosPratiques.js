a21343fcb0095835582f698d28bbb815f57546d6
Jordan Caumartin <pro.jordanc@gmail.com>
2019-12-13 16:10:00 +0100

    :bug: Fix css BUG, add instagram icon and link, fix Contact field

--- styleJSInfosPratiques.js
+++ styleJSInfosPratiques.js
@@ -1,44 +1,27 @@
 let button = document.querySelector(".toggleButton")
 let nav = document.querySelector(".nav")
 let overlay = document.querySelector("#overlay")
-var image = document.getElementById ("logoFb")
+var image = document.getElementById("logoFb")
 var largeur = window.innerWidth;
 
 
-button.onclick = function(){
-    nav.classList.toggle("navOpen")
-    overlay.classList.toggle("overlayOpen")
+button.onclick = function () {
+  nav.classList.toggle("navOpen")
+  overlay.classList.toggle("overlayOpen")
 }
 
-// window.onscroll = function() {scrollFunction()};
-
-
-// function scrollFunction() {
-// if (largeur > 376){
-
-//   }
-// if (largeur < 376){
-
-//     // if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
-//     //   document.getElementById("programmation").classList.add("showprogrammation");
-//     //   }
-//     // if (document.body.scrollTop > 3150 || document.documentElement.scrollTop > 3150) {
-//     //   document.getElementById("imgLocalisation").classList.add("imgLocalisationAfter");
-//     //   } 
-//   }
-// }
 
 var image = document.getElementById("photoCac");
 new simpleParallax(image, {
-        scale: 1.8
+  scale: 1.8
 });
-      
-if(largeur > 376){
-        document.getElementById("logoFb").src="img/Icon FB Bleu.png";
-        console.log("test");
 
+if (largeur > 376) {
+  document.getElementById("logoFb").src = "img/Icon FB Bleu.png";
+  document.getElementById("logoInsta").src = "img/2ndEdition/instagram.png";
 
 }
-else{
-        document.getElementById("logoFb").src="img/Icon FB blanc.png";
+else {
+  document.getElementById("logoFb").src = "img/Icon FB blanc.png";
+  document.getElementById("logoInsta").src = "img/Icon Insta Blanc.png";
 }
